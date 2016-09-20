214e02531eeaa89d1dba57010a17619bb2dc8dfd
HappySingh <happyislearning@gmail.com>
2016-09-20 01:23:04 -0400

    Add a new menu with an item for record and stream each

---
+++ app/pages/stream/home.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { AudioRecorder, AudioRecorderState } from '../../services/audiorecorder.ts';
+
+@Component({
+  templateUrl: 'build/pages/stream/home.html',
+  providers: [AudioRecorder]
+})
+export class StreamHomePage {
+
+  //media: AudioRecorder; 
+  AudioRecorderState = AudioRecorderState;
+
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              public audioRecorder: AudioRecorder) {
+    //this.audioRecorder.getMediaPlugin();
+  }
+  
+  startRecording(){
+  try{
+    this.audioRecorder.startRecording();
+  }
+  catch(e){
+    this.showAlert('Could not start recording '+e);
+  }
+}
+
+stopRecording(){
+  try{
+      this.audioRecorder.stopRecording();
+  }
+  catch(e){
+    this.showAlert('Could not stop recording');
+  }
+}
+
+startPlayback(){
+  try{
+      this.audioRecorder.startPlayback();
+  }
+  catch(e){
+    this.showAlert('Could not start audio playback');
+  }
+}
+
+stopPlayback(){
+  try{
+      this.audioRecorder.stopPlayback();
+  }
+  catch(e){
+    this.showAlert('Could not stop audio playback');
+  }
+}
+
+showAlert(message){
+  let alert = this.alertCtrl.create({
+    title: 'Error',
+    subTitle: message,
+    buttons: ['OK']
+  });
+  alert.present();
+}
+}
